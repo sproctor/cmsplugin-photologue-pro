64f11f16328704e5fa33f62db9111a4ca7676705
Gabriel - Iulian Dumbrava <gabriel.dumbrava@gmail.com>
2015-09-17 11:10:09 +0300

    Updated migrations and a few small fixes.

--- cmsplugin_photologue_pro/migrations/0001_initial.py
+++ cmsplugin_photologue_pro/migrations/0001_initial.py
@@ -1,96 +1,28 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Album'
-        db.create_table(u'cmsplugin_album', (
-            (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
-            ('album', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['photologue.Gallery'])),
-            ('per_page', self.gf('django.db.models.fields.IntegerField')(default=100, max_length=3)),
-        ))
-        db.send_create_signal(u'cmsplugin_photologue_pro', ['Album'])
+    dependencies = [
+        ('photologue', '0009_auto_20150907_2310'),
+        ('cms', '0012_auto_20150607_2207'),
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Album'
-        db.delete_table(u'cmsplugin_album')
-
-
-    models = {
-        'cms.cmsplugin': {
-            'Meta': {'object_name': 'CMSPlugin'},
-            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
-            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
-            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
-            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
-            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
-        },
-        'cms.placeholder': {
-            'Meta': {'object_name': 'Placeholder'},
-            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
-        },
-        u'cmsplugin_photologue_pro.album': {
-            'Meta': {'object_name': 'Album', 'db_table': "u'cmsplugin_album'", '_ormbases': ['cms.CMSPlugin']},
-            'album': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['photologue.Gallery']"}),
-            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
-            'per_page': ('django.db.models.fields.IntegerField', [], {'default': '100', 'max_length': '3'})
-        },
-        u'photologue.gallery': {
-            'Meta': {'ordering': "['-date_added']", 'object_name': 'Gallery'},
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'photos': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'galleries'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['photologue.Photo']"}),
-            'tags': ('tagging.fields.TagField', [], {}),
-            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
-            'title_slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
-        },
-        u'photologue.photo': {
-            'Meta': {'ordering': "['-date_added']", 'object_name': 'Photo'},
-            'caption': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'crop_from': ('django.db.models.fields.CharField', [], {'default': "'center'", 'max_length': '10', 'blank': 'True'}),
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'date_taken': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'effect': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'photo_related'", 'null': 'True', 'to': u"orm['photologue.PhotoEffect']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
-            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'tags': ('tagging.fields.TagField', [], {}),
-            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
-            'title_slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
-            'view_count': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'})
-        },
-        u'photologue.photoeffect': {
-            'Meta': {'object_name': 'PhotoEffect'},
-            'background_color': ('django.db.models.fields.CharField', [], {'default': "'#FFFFFF'", 'max_length': '7'}),
-            'brightness': ('django.db.models.fields.FloatField', [], {'default': '1.0'}),
-            'color': ('django.db.models.fields.FloatField', [], {'default': '1.0'}),
-            'contrast': ('django.db.models.fields.FloatField', [], {'default': '1.0'}),
-            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'filters': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'}),
-            'reflection_size': ('django.db.models.fields.FloatField', [], {'default': '0'}),
-            'reflection_strength': ('django.db.models.fields.FloatField', [], {'default': '0.6'}),
-            'sharpness': ('django.db.models.fields.FloatField', [], {'default': '1.0'}),
-            'transpose_method': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['cmsplugin_photologue_pro']
+    operations = [
+        migrations.CreateModel(
+            name='Album',
+            fields=[
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('per_page', models.IntegerField(default=100, verbose_name='Photos per page', choices=[(0, 'All'), (10, 10), (25, 25), (50, 50), (100, 100), (250, 250)])),
+                ('photos_to_show', models.IntegerField(default=5, verbose_name='Photos_to_show')),
+                ('album', models.ForeignKey(verbose_name='Album', to='photologue.Gallery')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('cms.cmsplugin',),
+        ),
+    ]
--- cmsplugin_photologue_pro/models.py
+++ cmsplugin_photologue_pro/models.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
 from cms.models import CMSPlugin
+from photologue import models as cms_models
 
 PHOTOS_PER_PAGE = (
     (0, _('All')),
@@ -17,7 +19,5 @@ class Album(CMSPlugin):
     """Model for Album CMS plugin."""
 
     album = models.ForeignKey('photologue.Gallery', verbose_name=_('Album'))
-    per_page = models.IntegerField(_('Photos per page'),
-                                   choices=PHOTOS_PER_PAGE, default=100,
-                                   max_length=3)
-    photos_to_show = models.IntegerField(_('Photos_to_show'), default=3)
+    per_page = models.IntegerField(_('Photos per page'), choices=PHOTOS_PER_PAGE, default=100)
+    photos_to_show = models.IntegerField(_('Photos_to_show'), default=getattr(cms_models, 'SAMPLE_SIZE', 6))
